95b9bcaef626c73d05834065ebb78c6f9d5d6536
Wolfram158 <116281245+Wolfram158@users.noreply.github.com>
2023-08-08 00:24:40 +0300

    Update product.hs

--- Scalar product of two real vectors/product.hs
+++ Scalar product of two real vectors/product.hs
@@ -1,1 +1,3 @@
 product (x:xs) (y:ys) = if xs == [] && ys == [] then x * y else if xs == [] || ys == [] then error "Vectors differ in length" else x * y + product xs ys
+
+product vector1 vector2 = if not ((length vector1) == (length vector2)) then error "Vectors differ in length" else (sum (map (\(x, y) -> x*y) (zip vector1 vector2)))
