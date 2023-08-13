bcd44ab4e2b232c75c499e64b1ff164e45a0d62b
Wolfram158 <116281245+Wolfram158@users.noreply.github.com>
2023-08-13 10:39:03 +0300

    Create functional_expression.hs

---
+++ Функциональные выражения/functional_expression.hs
@@ -0,0 +1,16 @@
+variable name = \args -> ((Map.fromList args) Map.! name)
+abstract f start = \exprs -> (\args -> (foldl f start (juxt args exprs)))
+add = abstract (+) 0
+multiply = abstract (*) 1
+f = constant 8
+g = constant 9
+h = variable "x"
+ss = add [f, g]
+mm = multiply [ss, h]
+i = variable "y"
+j = variable "z"
+cc = add [mm, ss, f, g, i, j]
+main = do
+        putStrLn (show (ss [("x", 3)]))
+        putStrLn (show (mm [("x", 10)]))
+        putStrLn (show (cc [("x", 5), ("y", 10), ("z", 15)]))
