4c18bd6b95d0545e8f3d0a2574c28dcedd6d658c
Wolfram158 <116281245+Wolfram158@users.noreply.github.com>
2023-08-19 16:25:21 +0300

    Update object_expression2.hs

--- Объектные выражения/object_expression2.hs
+++ Объектные выражения/object_expression2.hs
@@ -4,7 +4,7 @@ instance Show Expression where
   show (Constant value) = show value
   show (Variable name) = name
   show (Add (Constant x) (Constant y)) = show (x + y) {- ------> toMiniString -}
-   show (Add x y) = mconcat ["(", (show x), "+", (show y), ")"]
+  show (Add x y) = mconcat ["(", (show x), "+", (show y), ")"]
   show (Multiply x y) = mconcat [(show x), "*", (show y)]
 evaluate :: Expression -> [(String, Int)] -> Int
 evaluate (Add x y) args = (evaluate x args) + (evaluate y args)
