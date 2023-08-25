e35e96c82d5fd051fce65f25e80a8903955da465
Wolfram158 <116281245+Wolfram158@users.noreply.github.com>
2023-08-25 12:49:03 +0300

    Create product1.hs

---
+++ Scalar product of two real vectors/product1.hs
@@ -0,0 +1,8 @@
+product1 :: Num a => [a] -> [a] -> Maybe a
+product1 vec1 vec2
+       | length vec1 /= length vec2 || length vec1 == 0 = Nothing
+       | otherwise = Just $ sum $ zipWith (*) vec1 vec2
+main = do
+          putStrLn $ show $ product1 [1, 2, 3] [4, 5, 6]
+          putStrLn $ show $ product1 [1, 2, 3] [4, 5, 6, 7]
+          putStrLn $ show $ product1 [] []
